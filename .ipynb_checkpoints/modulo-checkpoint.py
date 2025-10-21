1fa85a508d7d6e330ff074399291c763010d8c02
NormanBeltran <josenormanbeltran@gmail.com>
2025-10-20 21:54:00 -0300

    Clase 2 - Funciones y Modulos

---
+++ .ipynb_checkpoints/modulo-checkpoint.py
@@ -0,0 +1,15 @@
+def cabecera():
+    print("Pagina: nn                            MICROSOFT                             Fecha dd/mm/aaaa")
+    print("                                Reporte nnnnnnnnnnnnnnnn")
+    print("____________________________________________________________________________________________")
+
+def pie():
+    print("____________________________________________________________________________________________")
+    print("Version:nn.nn                                                               Programa xxxxxxx")
+
+sucursales = ["Buenos Aires", "Cordoba", "Santa Fe", "Neuquen"]
+
+pi = 3.1416
+
+
+    
---
+++ biblioteca/modulo2.py
@@ -0,0 +1,8 @@
+def factorial(*args):
+    total = 1
+    for i in args:
+        total *= i
+
+    return total
+
+clientes = ["HSBC", "ITAU", "BANCO RIO", "CITIBANK"]
